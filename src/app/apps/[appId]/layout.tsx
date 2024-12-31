318ff2babe78eceaae20723601056b2e28d67f0b
Samir <samirdiff@proton.me>
2024-12-31 19:51:12 +0530

    Added metadata

--- src/app/apps/[appId]/layout.tsx
+++ src/app/apps/[appId]/layout.tsx
@@ -1,5 +1,17 @@
 import DashboardNav from "@/app/components/DashboardNav";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Automate Your System. Schedule Them with Ease.",
+  description:
+    "Effortlessly schedule and send webhooks at the perfect time—secure, reliable, and automated.",
+  icons: [
+    {
+      url: "/favicon.svg",
+    },
+  ],
+};
 
 export default function AppsTemplate({
   children,
