103426249e5630de34cbc2038de35c73a5913c86
Samir <samirdiff@proton.me>
2025-03-30 18:34:21 +0530

    fingers crossed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { MiddlewareConfig } from "next/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/webhooks(.*)", "/configure"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 
 export const config: MiddlewareConfig = {
-  matcher: ["/((?!.*\\..*|_next).*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+  ],
 };
